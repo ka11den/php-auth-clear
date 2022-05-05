f39ce2077d4e4d70f9cc32b9dee9115c65709ff7
ka1rex <90115256+ka1rex@users.noreply.github.com>
2022-05-05 19:12:56 +0400

    Update connect.php

--- vendor/connect.php
+++ vendor/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $connect = mysqli_connect('46.174.50.9', 'u8766_sigaretre', 'sC8hH4lR', 'u8766_sigaretre');
+    $connect = mysqli_connect('ip', 'login', 'password', 'login');
 
     if (!$connect) {
         die('Error connect to DataBase');
